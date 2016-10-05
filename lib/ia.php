ea50c71110271cfd029b4a789da7bcc8a3fb9e54
Andreu Sanchez Nunyez <ansanu@floridauniversitaria.es>
2016-10-05 13:11:07 +0200

    Proyecto finalizado

---
+++ lib/ia.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+   /**
+    * Andreu Clase nave filas y columnas
+    */
+   class ia
+   {
+
+      // Propiedades
+
+      public $columna=2 ;
+      public $fila=2 ;
+
+      //método fila y movimiento aleatorio
+      
+     public function getFila() {
+     
+     return $this->fila ;
+
+     }
+
+      //método columna y movimiento aleatorio
+
+     public function getColumna() {
+     
+     return $this->columna ;
+     
+     }
+
+   }
+
+   ?>
